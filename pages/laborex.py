70d54e4943e556652bffe8d4578666a6c513eb69
Sissoko <m.sissoko.7035@gmail.com>
2026-01-17 00:40:31 +0000

    redsign ok

--- pages/laborex.py
+++ pages/laborex.py
@@ -1,9 +1,60 @@
 import streamlit as st
 import pandas as pd
 from io import BytesIO
+from streamlit_option_menu import option_menu
 
-st.set_page_config(page_title="Extraction ventes par zone", layout="wide")
-st.title("📊 Extraction & Analyse des ventes par zone")
+st.set_page_config(page_title="Tableau de bord", page_icon="📊", layout="wide")
+
+
+st.title("MABOU BI ")
+st.write("Bienvenue dans votre application d'analyse de données.")
+
+
+
+# -------------------------------
+# Barre de navigation moderne
+# -------------------------------
+with st.sidebar:
+    st.image("assets/logo.png", width=120)
+    selected = option_menu(
+        "Navigation",
+        ["🏠 Tableau de bord", "📊 Extraction Ubipharm", "🧾 Extraction Laborex", ],
+        icons=["house", "bar-chart", "file-text"],
+        menu_icon="cast",
+        default_index=0,
+    )
+
+# -------------------------------
+# Logique de navigation
+# -------------------------------
+if selected == "🏠 Tableau de bord":
+   
+
+    st.subheader("⚙️ Actions rapides")
+    col1, col2 = st.columns(2)
+
+    with col1:
+        st.markdown("### 🧾 Extraction Ubipharm")
+        if st.button("➕ Nouvelle base"):
+            st.switch_page("pages/ubipharm_page.py")
+
+    with col2:
+        st.markdown("### 💰 Extraction Laborex")
+        if st.button("➕ Nouveau base"):
+            st.switch_page("pages/laborex.py")
+
+
+    st.markdown("---")
+  
+elif selected == "📊 Extraction Ubipharm": 
+    st.switch_page("pages/ubipharm_page.py")
+
+elif selected == "🧾 Extraction Laborex":
+    st.switch_page("pages/laborex.py")
+
+
+
+
 
 uploaded_file = st.file_uploader("📂 Charger le fichier Excel", type=["xlsx"])
 
--- pages/ubipharm_page.py
+++ pages/ubipharm_page.py
@@ -3,12 +3,60 @@ import pandas as pd
 from io import BytesIO
 
 from parsers.ubipharm import parse_ubipharm_txt
+from streamlit_option_menu import option_menu
 
-# --------------------------------------------------
-# CONFIG
-# --------------------------------------------------
-st.set_page_config(page_title="Ubipharm - Refactoring & Analyse", layout="wide")
-st.header("⚙️ Refactoring des données - Ubipharm")
+st.set_page_config(page_title="Tableau de bord", page_icon="📊", layout="wide")
+
+
+st.title("MABOU BI ")
+st.write("Bienvenue dans votre application d'analyse de données.")
+
+
+
+# -------------------------------
+# Barre de navigation moderne
+# -------------------------------
+with st.sidebar:
+    st.image("assets/logo.png", width=120)
+    selected = option_menu(
+        "Navigation",
+        ["🏠 Tableau de bord", "📊 Extraction Ubipharm", "🧾 Extraction Laborex", ],
+        icons=["house", "bar-chart", "file-text"],
+        menu_icon="cast",
+        default_index=0,
+    )
+
+# -------------------------------
+# Logique de navigation
+# -------------------------------
+if selected == "🏠 Tableau de bord":
+   
+
+    st.subheader("⚙️ Actions rapides")
+    col1, col2 = st.columns(2)
+
+    with col1:
+        st.markdown("### 🧾 Extraction Ubipharm")
+        if st.button("➕ Nouvelle base"):
+            st.switch_page("pages/ubipharm_page.py")
+
+    with col2:
+        st.markdown("### 💰 Extraction Laborex")
+        if st.button("➕ Nouveau base"):
+            st.switch_page("pages/laborex.py")
+
+
+    st.markdown("---")
+    st.caption("© 2026 MABOU-INSTRUMED - Système de gestion de données pharmaceutiques.")
+
+elif selected == "📊 Extraction Ubipharm": 
+    st.switch_page("pages/ubipharm_page.py")
+
+elif selected == "🧾 Extraction Laborex":
+    st.switch_page("pages/laborex.py")
+
+
+
 
 # --------------------------------------------------
 # INIT
